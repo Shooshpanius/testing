10c67d3e673917bdd5daf13ed3123a3eef5ecd9b
Shooshpanius <admin@in-da-house.ru>
2013-02-25 17:54:50 +0400

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,12 @@ Testing::Application.routes.draw do
     resources :users, :path => "/users/(:action)(.:format)"
   end
 
+
+  match "*path" => redirect("/login")
+
+
+
+
   #get 'admin/skills/list'
 
   #get ":admin/skills"
@@ -70,5 +76,5 @@ Testing::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
